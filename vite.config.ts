0f131a8677260f3eb6b7d61bfb3613999a889974
nyankieyapeter <146733144+nyankieyapeter@users.noreply.github.com>
2025-08-10 09:27:20 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/Mdrive-fleet-Services/", 
+  base: "", 
   server: {
     host: "::",
     port: 8080,
